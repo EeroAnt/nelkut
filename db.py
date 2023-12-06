f00ec315585935e74246edf91fb615edf2ed319a
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-12-06 18:24:51 +0200

    test stuff

--- db.py
+++ db.py
@@ -1,4 +1,14 @@
 
 from flask_sqlalchemy import SQLAlchemy
-
+from os import getenv
 db = SQLAlchemy()
+
+engine = db.create_engine(getenv("DATABASE"))
+
+
+def query(sql, params=None):
+	with engine.connect().execution_options(
+		isolation_level="AUTOCOMMIT"
+	) as connection:
+		with connection.begin():
+			connection.execute(sql, params or ())
